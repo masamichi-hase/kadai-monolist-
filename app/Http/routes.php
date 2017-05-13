b7fb609852c6c7f2cc81b334a901db97ab484c8b
masamichihase <drumming_o@yahoo.co.jp>
2017-05-13 12:09:20 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 Route::get('/', 'WelcomeController@index');
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
