afcc079276acda356968031043e6e3a7832ca2a4
masamichihase <drumming_o@yahoo.co.jp>
2017-05-15 14:01:40 +0000

    want2

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -74,7 +74,13 @@ class ItemController extends Controller
      */
     public function show($id)
     {
-        //
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
 
     /**
